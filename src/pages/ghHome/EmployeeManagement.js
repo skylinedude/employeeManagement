87c3f101bae9ce588eb7fbb7ed3edd9bf35e35ff
skylinedude <93267378+skylinedude@users.noreply.github.com>
2024-07-23 13:13:55 +0530

    third

--- src/pages/ghHome/EmployeeManagement.js
+++ src/pages/ghHome/EmployeeManagement.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
-import './loginStye.css'; 
+import './ghHome.css'; 
 
 const EmployeeManagemnt = () => {
     const [username, setUsername] = useState('');
